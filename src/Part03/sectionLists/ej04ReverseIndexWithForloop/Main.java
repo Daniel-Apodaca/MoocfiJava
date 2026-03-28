7583ef372b6ee10797dd6f9a1e093fd5d399b539
Daniel <jose.apodacaramirez@gmail.com>
2026-03-27 23:43:59 -0700

    Solucion ej04.5 Reverse Index WIth for loop

---
+++ src/Part03/sectionLists/ej04ReverseIndexWithForloop/Main.java
@@ -0,0 +1,22 @@
+package Part03.sectionLists.ej04ReverseIndexWithForloop;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        int index = numbers.size() - 1;
+
+        for(int i = index; i >= 0; i--) {
+            int number = numbers.get(i);
+            System.out.println(number);
+
+        }
+    }
+}
