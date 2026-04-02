ca9fa3626cfd28cd5d25c78fa90553bd7c0458e8
Daniel <jose.apodacaramirez@gmail.com>
2026-04-01 22:20:30 -0700

    Solucion ej 12 Sum of a List

---
+++ src/Part03/sectionLists/ej12SumOfAList/Main.java
@@ -0,0 +1,28 @@
+package Part03.sectionLists.ej12SumOfAList;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        int numbers = Integer.parseInt(input.nextLine());
+
+        while(numbers != -1) {
+            list.add(numbers);
+            numbers = Integer.parseInt(input.nextLine());
+        }
+
+        int sum = 0;
+
+        for(int numList: list) {
+            sum = numList + sum;
+        }
+
+        System.out.println("Sum: " + sum);
+        input.close();
+    }
+
+
+}
