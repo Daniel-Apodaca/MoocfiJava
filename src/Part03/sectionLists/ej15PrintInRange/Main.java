5e17c6a878d3c90338d7dae84bdddf1933382a1b
Daniel <jose.apodacaramirez@gmail.com>
2026-04-01 23:32:44 -0700

    Solucion ej 15 Print in range

---
+++ src/Part03/sectionLists/ej15PrintInRange/Main.java
@@ -0,0 +1,43 @@
+//INSTRUCCIONES
+//Create the method public static void printNumbersInRange(ArrayList<Integer> numbers,
+//                                                         int lowerLimit, int upperLimit)
+//in the exercise template. The method prints the numbers in the given list whose values
+//are in the range [lowerLimit, upperLimit]. A few examples of using
+//the method are supplied below.
+
+package Part03.sectionLists.ej15PrintInRange;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
+
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
+
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10);
+
+    }
+
+    public static void printNumbersInRange(
+            ArrayList<Integer> numbers,
+            int lowerLimit,
+            int upperLimit) {
+        for (int nums : numbers) {
+            if (nums >= lowerLimit && nums <= upperLimit) {
+                System.out.println(nums);
+            }
+        }
+    }
+
+
+}
