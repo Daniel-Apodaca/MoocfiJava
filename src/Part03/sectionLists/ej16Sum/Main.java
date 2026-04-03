0e4761b238f75dba954842af6d81ec3e2eef2b2e
Daniel <jose.apodacaramirez@gmail.com>
2026-04-02 22:48:51 -0700

    Solucion EJercicio 16 Sum

---
+++ src/Part03/sectionLists/ej16Sum/Main.java
@@ -0,0 +1,36 @@
+//INSTRUCCIONES
+//Create the method public static int sum(ArrayList<Integer> numbers)
+//in the exercise template. The method is to return the sum of the
+//numbers in the parameter list.
+
+package Part03.sectionLists.ej16Sum;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        System.out.println(sum(numbers));
+
+        numbers.add(5);
+        numbers.add(1);
+        System.out.println(sum(numbers));
+
+    }
+
+    public static int sum(ArrayList<Integer> numbers) {
+        int sum = 0;
+        for(int nums: numbers) {
+            sum += nums;
+        }
+        return sum;
+    }
+
+
+}
+
