0b97a5582e24b9a82cd63b8b91074adad36114ed
daniel-apodaca <jose.apodacaramirez@gmail.com>
2026-05-26 23:31:38 -0700

    Solucion ej 03 Sum of array

---
+++ src/Part03/Arrays/SumOfArray/Main.java
@@ -0,0 +1,25 @@
+//INSTRUCTIONS:
+//The class SumOfArray has a method public static int sumOfNumbersInArray(int[] array).
+//Complete the method so that it computes and returns the sum of the numbers in the array it
+//receives as parameter.
+
+package Part03.Arrays.SumOfArray;
+
+public class Main {
+    public static void main(String[] args) {
+    int[] numbers = {5, 1, 3, 4, 2};
+        System.out.println(sumOfNumbersInArray(numbers));
+    }
+
+    public static int sumOfNumbersInArray(int[] array) {
+       int sum = 0;
+        int index = 0;
+
+        while(index < array.length) {
+            sum = sum + array[index];
+            index++;
+        }
+
+        return sum;
+    }
+}
