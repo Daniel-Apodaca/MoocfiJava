587006e2042f6969287a0e2b5f1a29544d695b23
daniel-apodaca <jose.apodacaramirez@gmail.com>
2026-06-08 17:29:01 -0700

    Solucion ejercicio 04 Line by Line

---
+++ src/Part03/Strings/LineByLine/Main.java
@@ -0,0 +1,32 @@
+//Write a program that reads strings from the user. If the input is
+//        empty, the program stops reading input and halts.
+//        For each non-empty input it splits the string input by whitespaces
+//        and prints each part of the string on a new line.
+
+package Part03.Strings.LineByLine;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+
+
+        while (true) {
+            String text = input.nextLine();
+
+            if(text.isEmpty()) {
+            break;
+
+        } else {
+            String[] pieces = text.split(" ");
+            for (int i = 0; i < pieces.length; i++) {
+                System.out.println(pieces[i]);
+            }
+        }
+        }
+
+    }
+}
